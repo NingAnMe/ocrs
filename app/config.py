bd99b5069495d441014c4e868f5acc5a483c0105
NingAnMe <ninganme@qq.com>
2018-06-08 16:56:49 +0800

    修改 config 里面读取配置文件的关键字错误

--- app/config.py
+++ app/config.py
@@ -5,8 +5,10 @@
 @Author  : AnNing
 """
 
+from PB.pb_io import Config
 
-class GlobalConfig():
+
+class GlobalConfig(Config):
     """
     加载全局配置文件
     """
@@ -23,27 +25,27 @@ class GlobalConfig():
         try:
             # ###################IN#######################
             # 原始文件路径
-            self.l1_in_path = self.config_data['PATH']['IN']['dataset']
-            self.obc_in_path = self.config_data['PATH']['IN']['dataset']
-            self.ncep_in_path = self.config_data['PATH']['IN']['dataset']
-            self.coeff_in_path = self.config_data['PATH']['IN']['dataset']
+            self.l1_in_path = self.config_data['PATH']['IN']['l1']
+            self.obc_in_path = self.config_data['PATH']['IN']['obc']
+            self.ncep_in_path = self.config_data['PATH']['IN']['ncep']
+            self.coeff_in_path = self.config_data['PATH']['IN']['coeff']
 
             # ###################MID#######################
             # 中间文件的储存路径
-            self.calibrate_mid_path = self.config_data['PATH']['MID']['dataset']
-            self.incfg_mid_path = self.config_data['PATH']['MID']['dataset']
-            self.ncep_mid_path = self.config_data['PATH']['MID']['dataset']
-            self.granule_mid_path = self.config_data['PATH']['MID']['dataset']
-            self.projection_mid_path = self.config_data['PATH']['MID']['dataset']
+            self.calibrate_mid_path = self.config_data['PATH']['MID']['calibrate']
+            self.incfg_mid_path = self.config_data['PATH']['MID']['incfg']
+            self.ncep_mid_path = self.config_data['PATH']['MID']['ncep']
+            self.granule_mid_path = self.config_data['PATH']['MID']['granule']
+            self.projection_mid_path = self.config_data['PATH']['MID']['projection']
 
             # ###################OUT#######################
             # 输出的产品的存放路径
-            self.daily_out_path = self.config_data['PATH']['OUT']['dataset']
-            self.day_10_out_path = self.config_data['PATH']['OUT']['dataset']
-            self.monthly_out_path = self.config_data['PATH']['OUT']['dataset']
-            self.quarterly_out_path = self.config_data['PATH']['OUT']['dataset']
-            self.yearly_out_path = self.config_data['PATH']['OUT']['dataset']
-            self.log_out_path = self.config_data['PATH']['OUT']['dataset']
+            self.daily_out_path = self.config_data['PATH']['OUT']['daily']
+            self.day_10_out_path = self.config_data['PATH']['OUT']['day_10']
+            self.monthly_out_path = self.config_data['PATH']['OUT']['monthly']
+            self.quarterly_out_path = self.config_data['PATH']['OUT']['quarterly']
+            self.yearly_out_path = self.config_data['PATH']['OUT']['yearly']
+            self.log_out_path = self.config_data['PATH']['OUT']['log']
         except Exception as why:
             print why
             self.error = True
